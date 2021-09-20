ee6c288f3cb5f07bcb2e826fe80125e20e0088e5
Elise Muellerleile <eliamue@live.com>
2021-09-20 16:16:57 -0500

    search added

--- src/components/containers/GameCatalogClass.jsx
+++ src/components/containers/GameCatalogClass.jsx
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 import GameList from '../games/GameList';
 import { getGames } from '../services/gameApi';
+import Search from '../games/Search';
 
 export default class GameCatalogClass extends Component {
   state = {
     loading: true,
     games: [],
+    searchTerm: ''
   };
 
   async componentDidMount() {
     const games = await getGames();
     this.setState({ games, loading: false });
   }
+
+  handleSearch = (event) => {
+    console.log('handleSearch called');
+  }
+
   render() {
-    const { loading, games } = this.state;
+    const { loading, games, searchTerm } = this.state;
+
     if (loading) return <h1>Loading...</h1>;
 
-    return <GameList games={games} />;
+    return <>
+      <Search searchTerm={searchTerm} onSearch={this.handleSearch} />
+      <GameList games={games} />
+    </>;
   }
 }
